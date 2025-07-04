c951586f1459612a08169b93f804a6c1a6cf65c8
yash <ybvyas786@gmail.com>
2025-07-04 13:16:29 +0530

    class date 4 july

---
+++ Class_4_7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_BHUMI_MA_M
+{
+    internal class Class_4_7
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number : ");
+            int a = int.Parse(Console.ReadLine());  
+            Console.Write("Enter Second Number : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Sum of {a} and {b} is : {a + b}");
+
+        }
+    }
+}
